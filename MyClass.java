6d6d00ed7a00df4f8de0a3a9dec8d21032be9500
Nicholas Zufelt <nicholas.zufelt@gmail.com>
2017-02-20 14:34:00 -0500

    Initial Commit

---
+++ MyClass.java
@@ -0,0 +1,23 @@
+/*
+ * Initial code for first commit: stubs for methodA and methodB
+ * Author: Nicholas Zufelt
+ * Date: 2/20/2017
+ * Course: CSC630, Spring 2017
+ */
+
+public class MyClass {
+    // methodA will be completed by authorA
+    public static void methodA(){
+        // stub
+    }
+
+    // methodB will be completed by authorB
+    public static void methodB(){
+        // stub
+    }
+
+    public static void main(String[] args) {
+        methodA();
+        methodB();
+    }
+}
